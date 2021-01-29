c25ed5dde813a13004c9427102c090831003fdb4
Devmunds <devmunds@gmail.com>
2021-01-29 14:06:07 -0300

    Update: style

--- includes/wpme-functions.php
+++ includes/wpme-functions.php
@@ -5,22 +5,30 @@ function wpme_menu_user(){
     $current_user = wp_get_current_user();
 
     if (!is_user_logged_in() == false) {
-        $wpme_user = '<div id="wpme-login"><span>Olá, ' . $current_user->user_firstname . '</span></div>';
+        $wpme_user = '
+            <div class="nav-menu-login">
+                <ul class="wpme-menu-login">
+                    <li><a href="#"> Olá, ' . $current_user->user_firstname . '</a>
+                        <ul>
+                            <li><a href="/wp-login.php?action=logout">Sair</a></li>
+                        </ul>                
+                    </li>
+                </ul>
+            </div>'
+        ;        
     }
 
     if (is_user_logged_in() == false) {
         $wpme_user = '
             <div class="nav-menu-login">
-            <ul class="wpme-menu-login">
-                <li><a href="#">Área do Associado</a>
-                    <ul>
-                        <li><a href="/admin">Entrar</a></li>
-                        <li><a href="/wp-login.php?action=logout">Sair</a></li>
-                    </ul>                
-                </li>
-            </ul>
+                <ul class="wpme-menu-login">
+                    <li><a href="#">Área do Associado</a>
+                        <ul>
+                            <li><a href="/admin">Entrar</a></li>
+                        </ul>                
+                    </li>
+                </ul>
             </div>'
-
         ;
     }    
     return    $wpme_user;
